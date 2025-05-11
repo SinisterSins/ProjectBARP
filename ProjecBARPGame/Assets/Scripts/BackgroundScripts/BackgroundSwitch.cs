c12ba207ecceb86ab23517ab360342227a84410b
vanessagerbi <148285880+vanessagerbi@users.noreply.github.com>
2025-05-10 21:06:12 -0400

    Added background music

--- ProjecBARPGame/Assets/Scripts/BackgroundScripts/BackgroundSwitch.cs
+++ ProjecBARPGame/Assets/Scripts/BackgroundScripts/BackgroundSwitch.cs
@@ -4,6 +4,9 @@ public class BackgroundSwitch : MonoBehaviour
 {
     public BackgroundSetManager manager;
     public GameObject targetSet;
+    // Add variable for audio source
+    public AudioSource currentBackgroundMusic;
+    public AudioSource nextBackgroundMusic;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,9 +21,13 @@ public class BackgroundSwitch : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && manager !=null && targetSet != null)
+        if (other.CompareTag("Player") && manager != null && targetSet != null)
         {
+            //Trigger audio
+            currentBackgroundMusic.Stop();
+            nextBackgroundMusic.Play();
             manager.SetActiveSet(targetSet);
         }
+
     }
 }
